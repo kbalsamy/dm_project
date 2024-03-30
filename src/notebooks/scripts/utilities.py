09f1dd3213b02216b0a9a5f1b5d0d3ab5ab4e70a
kbalsamy <bkarthikonmail@gmail.com>
2024-03-30 16:07:52 +0000

    data preprocessing done

--- src/notebooks/scripts/utilities.py
+++ src/notebooks/scripts/utilities.py
@@ -89,3 +89,18 @@ def quantile_values(df, column, quantiles):
     return quantile_values
 
 
+def get_outliers_columns(df):
+    """
+    This function takes in a dataframe and returns a list of columns that have outliers.
+    """
+    outliers_columns = []
+    for column in df.select_dtypes(include=np.number).columns:
+        q1 = df[column].quantile(0.25)
+        q3 = df[column].quantile(0.75)
+        iqr = q3 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+        if len(outliers) > 0:
+            outliers_columns.append(column)
+    return outliers_columns
